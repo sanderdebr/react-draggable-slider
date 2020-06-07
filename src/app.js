55c4c7c4cbaeb162d283086214405e9296e9bc9d
sanderdebr <sander606@gmail.com>
2020-06-07 13:28:37 +0200

    webpack setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+// import DragSlider from "../components/DragSlider";
+// import ProjectItem from "../components/ProjectItem";
 
-export default function App() {
-    <h1>Hi</h1>
+export const projectList = [
+  {
+    title: "Eriden Al Global",
+    description:
+      "Eriden Global is a complete, cohesive and established group of Al focused software executives and developers with an impressive track record of success in applying AI and Machine Learning to solve incredibly complex business problems.",
+  },
+  {
+    title: "UnBatch.com",
+    description:
+      "UnBatch is an image and video sharing and discovery platform. As a simple new social networking site, we decided to follow the K.I.S.S philosophy. Keep Internet Sharing Simple. You share and we showcase to help you find cohorts cut from the same cloth.",
+  },
+  {
+    title: "BCC",
+    description:
+      "BCC is an image and video sharing and discovery platform. As a simple new social networking site, we decided to follow the K.I.S.S philosophy. Keep Internet Sharing Simple. You share and we showcase to help you find cohorts cut from the same cloth.",
+  },
+];
+
+export default function Projects(props) {
+  const amount = projectList.length;
+
+  return (
+    <ProjectSection>
+      {/* <DragSlider amount={amount}>
+        <Transition in={visible} timeout={0}>
+          {projectList.map(({ title, description }, index) => (
+            <ProjectItem
+              status={status}
+              title={title}
+              description={description}
+              index={index}
+            />
+          ))}
+        </Transition>
+      </DragSlider> */}
+    </ProjectSection>
+  );
 }
+
+const ProjectSection = styled.section`
+  width: auto;
+  height: 100vh;
+  border: 1px solid green;
+  overflow: visible;
+  position: relative;
+  background: purple;
+`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,29 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const pkg = require('./package.json');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [
-    `./src/app.js`,
-  ],
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: `app.js`,
-    library: pkg.name,
-    libraryTarget: 'commonjs2',
-  },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['babel-preset-env', 'babel-preset-react'],
-            }
-          }
-        ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.*css$/,
-        use : ExtractTextPlugin.extract({
-          fallback : 'style-loader',
-          use : [
-            'css-loader',
-            'sass-loader'
-          ]
-        })
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
       },
-    ]
-  },
-  externals: {
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
-    }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
+    ],
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: `${pkg.name}.css`,
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
     }),
   ],
 };
